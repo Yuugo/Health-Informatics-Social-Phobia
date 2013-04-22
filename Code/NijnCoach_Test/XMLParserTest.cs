e8d6b38346e43e712148290f1bb8ff79f95dd2f1
kjgm <kjgm_vd_linden@hotmail.com>
2013-04-22 10:51:23 +0200

    Test project toegevoegd

---
+++ Code/NijnCoach_Test/XMLParserTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NijnCoach;
+using System.IO;
+
+namespace NijnCoach_Test
+{
+    [TestFixture]
+    class XMLParserTest
+    {
+        [Test]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void invalidFile()
+        {
+            XMLParser parser = new XMLParser();
+            parser.readXML("");
+        }
+    }
+}
