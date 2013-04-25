8a3c8cf8be0e396842a678b2bbc9018f1d2cf4bf
kjgm <kjgm_vd_linden@hotmail.com>
2013-04-25 23:34:56 +0200

    Start of a GUI-test framework

---
+++ Code/NijnCoach_Test/GuiTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.Reflection;
+using System.Threading;
+using NUnit.Framework;
+
+namespace NijnCoach_Test
+{
+    abstract class GuiTest
+    {
+        /*With help of
+         * http://msdn.microsoft.com/en-us/magazine/cc188784.aspx
+         * http://stackoverflow.com/questions/1288310/activator-createinstance-how-to-create-instances-of-classes-that-have-paramete
+         */ 
+
+        Assembly testAssembly = null;
+        Form _testForm = null;
+        BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance | BindingFlags.FlattenHierarchy;
+
+        public Form testForm
+        {
+            get
+            {
+                return _testForm;
+            }
+        }
+
+        private static void RunApp(object state) 
+        { 
+            Application.Run((Form)state); 
+        } 
+        
+        private void _InvokeMethod(Form form, string methodName, params object[] parms)
+        { 
+            EventHandler eh = (EventHandler)Delegate.CreateDelegate(typeof (EventHandler), form, methodName); 
+            if (eh != null) { 
+                form.Invoke(eh, parms); 
+            }
+        } 
+
+        protected object GetField(object obj, string fieldName) 
+        { 
+            Type t = obj.GetType(); 
+            FieldInfo fi = t.GetField(fieldName, flags); 
+            return fi.GetValue(obj); 
+        } 
+
+        protected object GetProperty(object obj, string propertyName) 
+        { 
+            Type t = obj.GetType(); 
+            PropertyInfo pi = t.GetProperty(propertyName, flags); 
+            return pi.GetValue(obj, new object[0]); 
+        }
+
+        protected void InvokeMethod(String method)
+        {
+            object[] p = {this, new EventArgs()}; 
+            _InvokeMethod(_testForm, method, p); 
+        }
+
+        public object GetPropertyFromFromField(string fieldName, string propertyName)
+        {
+            return GetProperty(GetField(_testForm, fieldName), propertyName);
+        }
+
+
+
+        public abstract object[] getParameters();
+        public abstract String getAssembly();
+        public abstract String getForm();
+
+        [TestFixtureSetUp]
+        public virtual void setUp()
+        {
+            testAssembly = Assembly.LoadFrom(getAssembly());
+            Type t = testAssembly.GetType(getForm());
+            Type[] pTypes = Type.GetTypeArray(getParameters());
+            ConstructorInfo ctor = t.GetConstructor(pTypes);
+            _testForm = ctor.Invoke(getParameters()) as Form;
+            ThreadPool.QueueUserWorkItem(new WaitCallback(RunApp), _testForm);
+        }
+
+        [TestFixtureTearDown]
+        public virtual void tearDown()
+        {
+            _testForm.Dispose();
+            _testForm.Close();
+        }
+
+    }
+}
---
+++ Code/NijnCoach_Test/PatientQuestionnaireGUITest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.Reflection;
+using System.Threading;
+using NUnit.Framework;
+using NijnCoach.View.Questionnaire;
+using NijnCoach.XMLclasses;
+using NijnCoach;
+
+namespace NijnCoach_Test
+{
+    [TestFixture]
+    class PatientQuestionnaireGUITest: GuiTest
+    {
+        private const string xmlFile = "writeTest.xml";
+        private XMLParser parser = null;
+        private object[] _parameters = null;
+        private String _assembly = "NijnCoach.exe";
+        private String _form = "NijnCoach.View.Questionnaire.QuestionnaireForm";
+
+        public override object[] getParameters()
+        {
+            return _parameters;
+        }
+
+        public override String getAssembly()
+        {
+            return _assembly;
+        }
+
+        public override String getForm()
+        {
+            return _form;
+        }
+
+        [TestFixtureSetUp]
+        public override void setUp()
+        {
+            parser = new XMLParser();
+            Questionnaire theForm = parser.readXMLFromFile(xmlFile);
+            _parameters = new object[1] {theForm};
+            base.setUp();
+        }
+
+        [Test]
+        public void commentTest()
+        {
+            object panelQuestion = GetField(testForm, "panelQuestionIntern");
+            object labelComment = GetField(panelQuestion, "labelComment");
+            object commentText = GetProperty(labelComment, "Text");
+            Assert.AreEqual("blabla", commentText);
+            //InvokeMethod("nextEventHandler");
+        }
+
+
+    }
+}
