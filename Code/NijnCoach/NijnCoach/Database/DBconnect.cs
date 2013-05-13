3e8cad83c0a90bb2e6403c9e263f5edef27449e1
Yuugo <hugoreinbergen@hotmail.com>
2013-05-13 10:07:07 +0200

    gaaahhh

--- Code/NijnCoach/NijnCoach/Database/DBconnect.cs
+++ Code/NijnCoach/NijnCoach/Database/DBconnect.cs
@@ -2,11 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
+using NijnCoach.XMLclasses;
 
 namespace NijnCoach.Database
 {
-    class DBconnect
+    class DBConnect
     {
-        
+        public Boolean insertPatient(Patient patient)
+        {
+            NijnCoachEntities2 theEntities = new NijnCoachEntities2();
+            try
+            {
+                theEntities.Patients.AddObject(patient);
+                theEntities.SaveChanges();
+                return true;
+                 
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.InnerException.ToString());
+                return false;
+            }
+        }
+
+        public Boolean InsertQuestionnairre(String name, String text)
+        {
+            NijnCoachEntities2 theEntities = new NijnCoachEntities2();
+            Questionnairre questionForm = new Questionnairre();
+            questionForm.Name = name;
+            questionForm.Text = text;
+            try
+            {
+                theEntities.Questionnairres.AddObject(questionForm);
+                theEntities.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.InnerException.ToString());
+                return false;
+            }
+        }
+
+        public Boolean InsertSpeechFile(Object questionForm)
+        {
+            NijnCoachEntities2 theEntities = new NijnCoachEntities2();
+            try
+            {
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public void updateQuery()
+        {
+            NijnCoachEntities2 theEntities = new NijnCoachEntities2();
+            try
+            {
+                Patient patient = theEntities.Patients.First<Patient>();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.InnerException.ToString());
+            }
+        }
     }
 }
