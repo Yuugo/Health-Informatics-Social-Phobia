7840e0f2cbff3062faf933726be5a5dcd32597dd
Johnny Verhoeff <j.verhoeff@me.com>
2013-05-16 08:57:37 +0200

    test

--- Code/NijnCoach/NijnCoach/View/QuestionnaireForm/TestQuestionnaireForm.cs
+++ Code/NijnCoach/NijnCoach/View/QuestionnaireForm/TestQuestionnaireForm.cs
@@ -1,34 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
-using NijnCoach.XMLclasses;
-using NijnCoach.View.Questionnaire;
 
-namespace NijnCoach
-{
-    static class TestQuestionnaireForm
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new QuestionnaireForm());
-
-            Application.Run(new NijnCoach.View.TherapistGUI.TherapistGUI());
-            
-            //questionnaire = parser.readXMLFromFile("answers.xml");
-            //Application.Run(new TherapistQuestionnaire(questionnaire));
-            
-        }
-    }
-}
-=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -58,4 +28,3 @@ namespace NijnCoach
         }
     }
 }
->>>>>>> test
