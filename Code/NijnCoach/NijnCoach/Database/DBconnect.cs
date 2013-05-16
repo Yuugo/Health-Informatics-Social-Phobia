19731eccfa8268e16e0bf58b1e5caa3b34fb2dd4
Yuugo <hugoreinbergen@hotmail.com>
2013-05-16 09:10:09 +0200

    Quick commit for willem.

--- Code/NijnCoach/NijnCoach/Database/DBconnect.cs
+++ Code/NijnCoach/NijnCoach/Database/DBconnect.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Windows.Forms;
 using NijnCoach.XMLclasses;
 using System.Xml;
+using System.IO;
 
 namespace NijnCoach.Database
 {
@@ -35,8 +36,6 @@ namespace NijnCoach.Database
 
             questionForm.Name = name;
             questionForm.Text = text;
-            
-
             try
             {
                 theEntities.Questionnairres.AddObject(questionForm);
@@ -55,6 +54,7 @@ namespace NijnCoach.Database
             NijnCoachEntities2 theEntities = new NijnCoachEntities2();
             try
             {
+                
                 return true;
             }
             catch (Exception e)
@@ -86,6 +86,27 @@ namespace NijnCoach.Database
             return result;
         }
         #endregion
+        /// <summary>
+        /// Finds the questionnaire with the given name. Throws FileNotFoundException if it does not exist.
+        /// </summary>
+        /// <param name="name">Name of the questionnaire</param>
+        /// <returns>The Questionnaire</returns>
+        public Questionnaire getQuestionnaireByName(String name)
+        {
+            try
+            {
+                NijnCoachEntities2 theEntities = new NijnCoachEntities2();
+                String result = theEntities.Questionnairres.Where(x => x.Name == name).First<Questionnairre>().Text;
+                XMLParser parser = new XMLParser();
+                StreamReader reader = new StreamReader(result);
+                return parser.readXML(reader);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.InnerException.ToString());
+                throw new FileNotFoundException();
+            }
+        }
 
         public void updateQuery()
         {
---
+++ Code/NijnCoach/NijnCoach/MainClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NijnCoach.XMLclasses;
+
+namespace NijnCoach
+{
+    class MainClass
+    {
+        public static void Main(String[] args)
+        {
+            Questionnaire q = new Questionnaire();
+            //DBconnect conn = new DBconnect();
+        }
+    }
+}
