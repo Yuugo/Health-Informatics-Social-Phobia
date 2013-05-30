ae8c44b7293b11913dab50829e1c2dbdc5d430fb
Elgar <elgar.groot@gmail.com>
2013-05-30 12:33:12 +0200

    Datagenerator updated and used to create dummy exposure session files

--- Code/NijnCoach/NijnCoach/View/Overview/DataGenerator.cpp
+++ Code/NijnCoach/NijnCoach/View/Overview/DataGenerator.cpp
@@ -1,15 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+int sud(int x, double delta){
+	double a = -0.000005;
+	double b = x-900;
+	double c = 5 * delta;
+	double r = a*b*b+c;
+	return (int)r;
+}
+
+int gsr(int x, double delta){
+	double a = -0.0001;
+	double b = x-900;
+	double c = 545 * delta;
+	double r = a*b*b+c;
+	return (int)r;
+}
+
+int hr (int x, double delta){
+	double a = -0.00003;
+	double b = x-900; 
+	double c = 75 * delta;
+	double r = a*b*b+c; 
+	return (int)r; 
+}
+
 int main()
 {
     srand(time(NULL));
     printf("TimeStamp\tHeartRate\t\tGSR\t\tSUD\n");
-    int hour =   rand() %24; 
-    int minute = rand()%60;
-    int second = rand()%60;
-    int i;
-    for(i = 0;i<1800;i++){
+    int hour =   15;
+    int minute = 00;
+    int second = 00;
+	double delta = 1.50;
+	
+    for(int i = 0;i<1800;i++){
         second++;
         if(second>=60){
           second = 0;
@@ -23,10 +49,10 @@ int main()
           hour = 0;
         }
         printf("%02d:%02d:%02d\t",hour,minute,second);
-        printf("%d\t",65+rand()%20);//heartrate
-        printf("%d\t",860+rand()%40);//GSR
+        printf("%d\t",hr(i,delta));//heartrate
+        printf("%d\t",gsr(i,delta));//GSR
         if(minute%3==0&&second==0)
-         printf("%d",1+rand()%10);//SUD
+         printf("%d",sud(i,delta));//SUD
         printf("\n");
     }
 }
