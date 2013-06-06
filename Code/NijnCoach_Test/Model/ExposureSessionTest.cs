7a18365cc2d60e6209a91bfd4a74a699240f47ee
Elgar <elgar.groot@gmail.com>
2013-06-06 11:38:30 +0200

    Unit test for exposure session added

--- Code/NijnCoach_Test/Model/ExposureSessionTest.cs
+++ Code/NijnCoach_Test/Model/ExposureSessionTest.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
-using NUnit.Mocks;
 using NijnCoach;
 using System.IO;
 using NijnCoach.Exposure_data;
+using Moq;
 
 namespace NijnCoach_Test.Model
 {
@@ -20,6 +20,22 @@ namespace NijnCoach_Test.Model
         {
         }
 
+        private void initEmptySession()
+        {
+            session = new ExposureSession(new DateTime(2013, 1, 1));
 
+
+        }
+
+        private void initProperSession()
+        {
+            session = new ExposureSession(new DateTime(2013, 1, 1));
+
+            var stamp1 = new Mock<ExpTimeStamp>();
+            var stamp2 = new Mock<ExpTimeStamp>();
+            var stamp3 = new Mock<ExpTimeStamp>();
+
+
+        }
     }
 }
