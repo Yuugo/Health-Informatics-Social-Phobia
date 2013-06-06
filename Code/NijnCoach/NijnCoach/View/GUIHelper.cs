29240f3e5beaa6a778f64e1706f200fccf5bda56
kjgm <kjgm_vd_linden@hotmail.com>
2013-06-06 10:13:48 +0200

    Refactored greetpanel and homepanel

---
+++ Code/NijnCoach/NijnCoach/View/GUIHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NijnCoach.View
+{
+    class GUIHelper
+    {
+        public static void setElement<T>(ref T control, System.Drawing.Point location, String name, System.Drawing.Size size,
+                int tabIndex, String text, Boolean enabled = true) where T : System.Windows.Forms.Control
+        {
+            control.Location = location;
+            control.Name = name;
+            control.Size = size;
+            control.TabIndex = tabIndex;
+            control.Text = text;
+            control.Enabled = enabled;
+        }
+    }
+}
