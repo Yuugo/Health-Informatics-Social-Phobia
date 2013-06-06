3619152b4264357bb2dad50b5c1013cf05af6ad5
kjgm <kjgm_vd_linden@hotmail.com>
2013-06-06 10:40:51 +0200

    GuiHelper in Project

--- Code/NijnCoach/NijnCoach/View/GUIHelper.cs
+++ Code/NijnCoach/NijnCoach/View/GUIHelper.cs
@@ -8,7 +8,8 @@ namespace NijnCoach.View
     class GUIHelper
     {
         public static void setElement<T>(ref T control, System.Drawing.Point location, String name, System.Drawing.Size size,
-                int tabIndex, String text, Boolean enabled = true) where T : System.Windows.Forms.Control
+                int tabIndex, String text, Boolean enabled = true) 
+            where T : System.Windows.Forms.Control
         {
             control.Location = location;
             control.Name = name;
