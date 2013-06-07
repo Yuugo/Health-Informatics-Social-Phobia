3d6939a97cb5b455c1431849b6e7b81509519b0b
Elgar <elgar.groot@gmail.com>
2013-06-07 12:59:32 +0200

    Continued testing

--- Code/NijnCoach_Test/Model/ExpTimeStampTest.cs
+++ Code/NijnCoach_Test/Model/ExpTimeStampTest.cs
@@ -13,55 +13,52 @@ namespace NijnCoach_Test.Model
     [TestFixture]
     class ExpTimeStampTest
     {
-
+        protected Boolean isRightTimeStamp(ExpTimestamp s, int sud, int gsr, int hr, DateTime time)
+        {
+            return s.getSUD() == sud &&
+                    s.getGSR() == gsr &&
+                    s.getHR() == hr &&
+                    s.getTime() == time;
+        }
 
         [Test]
         public void validStampRegularTimeNoSUDReadTest()
         {
-            string stamp = "15:00:01    90  900";
+            string stamp = "15:00:01  90  900";
             DateTime d = new DateTime(2013, 01, 01, 15, 00, 00);
             
             ExpTimestamp testOb = ExpTimestamp.ReadExpTimestamp(stamp, d);
 
-            Assert.AreEqual(90, testOb.getHR());
-            Assert.AreEqual(900, testOb.getGSR());
-            Assert.AreEqual(-1, testOb.getSUD());
-            Assert.AreEqual(new DateTime(2013,01,01,00,00,01), testOb.getTime());
+            Assert.IsTrue(isRightTimeStamp(testOb, -1, 900, 90, new DateTime(2013,01,01,00,00,01)));
         }
 
         [Test]
         public void validStampOddTimeReadTest()
         {
-            string stamp = "16:01:00    90  900";
+            string stamp = "16:01:00  90  900";
             DateTime d = new DateTime(2013, 01, 01, 15, 59, 59);
 
             ExpTimestamp testOb = ExpTimestamp.ReadExpTimestamp(stamp, d);
 
-            Assert.AreEqual(90, testOb.getHR());
-            Assert.AreEqual(900, testOb.getGSR());
-            Assert.AreEqual(-1, testOb.getSUD());
-            Assert.AreEqual(new DateTime(2013, 01, 01, 00, 01, 01), testOb.getTime());
+            Assert.IsTrue(isRightTimeStamp(testOb, -1, 900, 90, new DateTime(2013, 01, 01, 00, 01, 01)));
         }
 
         [Test]
         public void validStampWithSUDReadTest()
         {
-            string stamp = "15:00:01    88  736 3";
+            string stamp = "15:00:01  90  900 3";
             DateTime d = new DateTime(2013, 01, 01, 15, 00, 00);
 
             ExpTimestamp testOb = ExpTimestamp.ReadExpTimestamp(stamp, d);
 
-            Assert.AreEqual(88, testOb.getHR());
-            Assert.AreEqual(736, testOb.getGSR());
-            Assert.AreEqual(3, testOb.getSUD());
-            Assert.AreEqual(new DateTime(2013, 01, 01, 00, 00, 01), testOb.getTime());
+            Assert.IsTrue(isRightTimeStamp(testOb, 3, 900, 90, new DateTime(2013, 01, 01, 00, 00, 01)));
         }
 
         [Test]
-        [ExpectedException(typeof(InvalidOperationException))]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void validStampFaultyTimeReadTest()
         {
-            string stamp = "25:61:61    88  736";
+            string stamp = "25:61:61  90  900";
             DateTime d = new DateTime(2013, 01, 01, 15, 00, 00);
 
             ExpTimestamp testOb = ExpTimestamp.ReadExpTimestamp(stamp, d);
