be4ab770bffe74606e57c8140de2396f170b7dc4
kjgm <kjgm_vd_linden@hotmail.com>
2013-06-13 10:16:26 +0200

    Overviewpanel for therapist

---
+++ Code/NijnCoach/NijnCoach/View/TherapistGUI/OverviewPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NijnCoach.View.TherapistGUI
+{
+    class OverviewPanel : NijnCoach.View.Overview.OverviewPanel
+    {
+        public OverviewPanel() : base(false)
+        {
+
+        }
+        
+        protected override void homeEventHandler(object sender, EventArgs e)
+        {
+            TherapistGUI.TherapistMain.main.replacePanel(new TherapistGUI.HomePanel());
+        }
+    }
+}
