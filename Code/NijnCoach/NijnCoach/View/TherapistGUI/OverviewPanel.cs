795ebf299020108de1c501fabb991826603167d9
kjgm <kjgm_vd_linden@hotmail.com>
2013-06-13 11:08:26 +0200

    OverviewPanel for therapist

--- Code/NijnCoach/NijnCoach/View/TherapistGUI/OverviewPanel.cs
+++ Code/NijnCoach/NijnCoach/View/TherapistGUI/OverviewPanel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NijnCoach.XMLclasses;
+using NijnCoach.View.Questionnaire;
 
 namespace NijnCoach.View.TherapistGUI
 {
@@ -16,6 +18,12 @@ namespace NijnCoach.View.TherapistGUI
         {
             base.InitializeComponent();
             //TODO: Resize some of the components
+            commentPanel.Dispose();
+            commentPanel = new OpenQuestionPanel(550, 100);
+            commentPanel.Size = new System.Drawing.Size(550, 100);
+            OpenQuestion oq = new OpenQuestion();
+            oq.question = "What comment do you want to give to this patient?";
+            (commentPanel as OpenQuestionPanel).entry = oq;
         }
         
         protected override void homeEventHandler(object sender, EventArgs e)
